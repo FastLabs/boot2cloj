54ce883cb7a50a408ed26cf0c70b4291d4cc734d
FastLabs <bulavitchi@gmail.com>
2013-09-12 23:52:51 +0100

    extended the protobuf samples, fixed the namming issue

--- proto.clj
+++ proto.clj
@@ -35,16 +35,30 @@
 
 ;;
 
+
 (use 'flatland.protobuf.core)
 (import zorg.PersonMessage)
 (import zorg.Persona)
 
-(def PersMessage (protodef zorg.PersonMessage))
+(def PersMessage (protodef zorg.PersonMessage {:naming-strategy  (reify flatland.protobuf.PersistentProtocolBufferMap$Def$NamingStrategy
+                          (protoName [this clojure-name]
+                            (name clojure-name))
+                          (clojureName [this proto-name]
+                            (keyword proto-name)))}))
+
 (def Pers (protodef zorg.Persona))
+(def Telephone (protodef zorg.Telephone))
+(def Address (protodef zorg.Address))
+(def Family (protodef zorg.Family))
 
 (def pm (protobuf PersMessage
                   :sex :MALE
-                  :persona (protobuf Pers :id 1 :name "Oleg")))
+                  :persona (protobuf Pers :id 1 :name "Oleg"
+                                     :address (protobuf Address :street "Oak way"
+                                                        :postcode "TW14 8AT"
+                                                        :telephone (protobuf Telephone :area "11")))
+                  :myFamily (protobuf Family :name "zozo")))
+
 (def saved (protobuf-dump pm))
 (def loaded (protobuf-load PersMessage saved))
 loaded
